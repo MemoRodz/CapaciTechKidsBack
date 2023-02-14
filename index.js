fec6212b1a7edc87b5c1a591110d2fdfcf32a5ed
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-02-14 09:58:15 -0500

    add/contenido carpeta App > Inclusión de middlewares necesarios para comenzar a trabajar el back > creación de server > establecimiento de sync en false para las tablas desde index.js >

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-//Fisrt commit
+const server = require("./src/App")
+const PORT = process.env.PORT
+const {sequelize} = require ("./DB_connection.js")
+
+sequelize.sync({ force: true }).then(() => {
+    
+})
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`)
+});
