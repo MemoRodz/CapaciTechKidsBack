e9536093a94860661dc7fc8cd3740ef228f4ad37
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-02-21 20:48:14 -0500

    act

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -13,7 +13,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY, DB_DB } = process.env;
 
 // postgres://DB_USER:DB_PASSWORD@DB_HOST/NOMBREBASEDEDATOS ESTRUCTURA DATOS SEQUELIZE
 
- const sequelize = new Sequelize(DB_DEPLOY,
+ const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DB}`,
    // URL
    { 
    logging: false, 
