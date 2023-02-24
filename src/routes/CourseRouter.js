80301a83573bf37b3b3d1404d31c982a193d5448
Fabian Rizzi <faberizzi92@gmail.com>
2023-02-24 16:22:45 +0100

    Rutas actualizadas de cursos. Función desactivadora de cursos

--- src/routes/CourseRouter.js
+++ src/routes/CourseRouter.js
@@ -13,6 +13,54 @@ CourseRouter.get("/", async(req,res) => {
     
 try{
     const result = await tblCourses.findAll({
+        where: {Active: true},
+            include: [
+                { model:tblUsers,
+                
+                },
+                { model: tblCategories,
+                  
+                },
+                {model: tblReviews,
+                }
+            ]
+    });
+
+    res.status(200).json(result)
+        }
+catch (error) {
+    res.status(400).send(error.message)
+        }
+})
+CourseRouter.get("/deleted", async(req,res) => {
+    
+try{
+    const result = await tblCourses.findAll({
+        where: {Active: false},
+            include: [
+                { model:tblUsers,
+                
+                },
+                { model: tblCategories,
+                  
+                },
+                {model: tblReviews,
+                }
+            ]
+    });
+
+    res.status(200).json(result)
+        }
+catch (error) {
+    res.status(400).send(error.message)
+        }
+})
+
+
+CourseRouter.get("/all", async(req,res) => {
+    
+try{
+    const result = await tblCourses.findAll({
         
             include: [
                 { model:tblUsers,
@@ -38,6 +86,32 @@ CourseRouter.get("/detail/:id", async (req,res) =>{
    
     try{
         const result = await getDetails(id)
+       
+         res.status(200).json(result)
+             }
+     catch (error) {
+         res.status(400).send(error.message)
+             }
+     })
+
+CourseRouter.put("/detail/:id/delete", async (req,res) =>{
+    const {id} = req.params;
+   
+    try{
+        const result = await tblCourses.update({ Active: false}, {where: {PK_Course: id}})
+       
+         res.status(200).json(result)
+             }
+     catch (error) {
+         res.status(400).send(error.message)
+             }
+     })
+
+CourseRouter.put("/detail/:id/activate", async (req,res) =>{
+    const {id} = req.params;
+   
+    try{
+        const result = await tblCourses.update({ Active: true}, {where: {PK_Course: id}})
        
          res.status(200).json(result)
              }
