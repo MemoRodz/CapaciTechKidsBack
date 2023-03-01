e86f1f04e8635597edc4893c3f25fe2c5ef8f971
Brareyesb15 <116505799+Brareyesb15@users.noreply.github.com>
2023-03-01 11:56:58 -0500

    lectures

---
+++ src/controllers/saveLectures.js
@@ -0,0 +1,25 @@
+const {tblCourses,tblUsers,tblLectures} = require("../DB_connection.js");
+const realDB = require("../utils/DBReal.js");
+const categoriesXCourses = require("./postCategoriesXCourse.js");
+
+
+const getFalseApiToDB = async() => {
+ 
+try {
+
+    await tblCourses.bulkCreate(realDB)
+
+    const allCourses = await tblCourses.findAll()
+   for (let i = 0; i < allCourses.length; i++) {
+        categoriesXCourses(allCourses[i].PK_Course, realDB[i].Category)}
+
+        
+    
+  
+    }
+    catch(error){
+        return error.message
+    }
+}
+
+module.exports = getFalseApiToDB;
