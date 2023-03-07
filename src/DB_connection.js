ffa2d460e3765cd71780b3c9bca43159c191f763
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-03-07 14:07:43 -0300

    asd..

--- src/DB_connection.js
+++ src/DB_connection.js
@@ -13,20 +13,20 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY, DB_DB } = process.env;
 
 // postgres://DB_USER:DB_PASSWORD@DB_HOST/NOMBREBASEDEDATOS ESTRUCTURA DATOS SEQUELIZE
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DB}`,
-   // URL
-//   { 
-//   logging: false, 
-//   native: false 
-//}
-//);
+//  const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DB}`,
+//    // URL
+//    { 
+//    logging: false, 
+//    native: false 
+// }
+// );
 
- const sequelize = new Sequelize(DB_DEPLOY,
-    {
-       logging: false,
-       native: false
-    }
- );
+const sequelize = new Sequelize(DB_DEPLOY,
+   {
+      logging: false,
+      native: false
+   }
+);
 
 //Acá irán las funciones de los modelos. ej: modelStudents(sequelize)
 
