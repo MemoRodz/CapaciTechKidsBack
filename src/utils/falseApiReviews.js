693874197a181b21f8d0b17ae571bacd914e371c
Agustin cristobo <75025925+Fr33yr@users.noreply.github.com>
2023-03-07 18:35:32 -0300

    Base de datos final final ahora si

--- src/utils/falseApiReviews.js
+++ src/utils/falseApiReviews.js
@@ -1,33 +1,10 @@
-const falseReviews = [{
-    Score : 10,
-    Comment : "Excelente curso, me fascinó",
-    PK_Course : 6,
-    PK_User : 7
-},
-{
-    Score : 2,
-    Comment : "Malo",
-    PK_Course : 5,
-    PK_User : 4
-},
-{
-    Score : 7,
-    Comment : "bueno pero no el mejor",
-    PK_Course : 8,
-    PK_User : 3
-},
-{
-    Score : 1,
-    Comment : "perdí mi tiempo",
-    PK_Course : 1,
-    PK_User : 5
-},
-{
-    Score : 8,
-    Comment : "Muy bueno, mi hijo aprendó todo sobre este tema",
-    PK_Course : 5,
-    PK_User : 2
-}
+const falseReviews = [
+    {
+        Score: 7,
+        Comment: "bueno pero no el mejor",
+        PK_Course: 8,
+        PK_User: 3
+    },
 ]
 
 module.exports = falseReviews;
